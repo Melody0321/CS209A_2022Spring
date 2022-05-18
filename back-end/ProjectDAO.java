f0240ada477e82a7d0ef8ce1267872c352ab6417
LiuJD <68370581+Melody0321@users.noreply.github.com>
2022-05-18 19:35:22 +0800

    Add files via upload

---
+++ back-end/ProjectDAO.java
@@ -0,0 +1,17 @@
+package com.example.demo.dao;
+import com.example.demo.pojo.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+public interface ProjectDAO extends JpaRepository<Project, Long> {
+    List<Project> findAll();
+    @Query(value="with a as(select distinct project_id as id from project_topic\n" +
+            "where topic like '%an%'\n" +
+            "union\n" +
+            "select distinct id\n" +
+            "from project\n" +
+            "where name like '%an%')\n" +
+            "select * from project,a where project.id=a.id",nativeQuery = true)
+    List<Project> findByProjectInfo(String name, String tag);
+}
---
+++ back-end/ProjectService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+import com.example.demo.dao.ProjectDAO;
+import com.example.demo.pojo.Project;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class ProjectService {
+    @Autowired
+    ProjectDAO projectDAO;
+    public List<Project> findAll(){
+        return projectDAO.findAll();
+    }
+    public List<Project> findByProjectInfo(String name, String tag){
+        String realName="%";
+        String realTag="%";
+        if(Objects.equals(name, "")){
+            realName="%";
+            realTag="%"+tag+"%";
+        }
+        if (Objects.equals(tag, "")){
+            realTag="%";
+            realName="%"+name+"%";
+        }
+        return projectDAO.findByProjectInfo(realName, realTag);
+    }
+}
