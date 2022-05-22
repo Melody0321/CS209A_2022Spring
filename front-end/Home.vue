82ed40e8379820951e453cac15fe74e74649e31c
LiuJD <68370581+Melody0321@users.noreply.github.com>
2022-05-22 16:54:27 +0800

    Update Home.vue

--- front-end/Home.vue
+++ front-end/Home.vue
@@ -129,33 +129,33 @@ export default {
         legend: {               //设置区分（哪条线属于什么）
           data: ['Number of new projects', 'Total number']
         },
-        color: ['#8AE09F', '#FA6F53'],       //设置区分（每条线是什么颜色，和 legend 一一对应）
+        color: ['blue', '#FA6F53'],       //设置区分（每条线是什么颜色，和 legend 一一对应）
         xAxis: {                //设置x轴
           type: 'category',
           boundaryGap: false,     //坐标轴两边不留白
           data: ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022'],
           name: 'DATE',           //X轴 name
           nameTextStyle: {        //坐标轴名称的文字样式
-            color: '#FA6F53',
+            color: 'black',
             fontSize: 16,
             padding: [0, 0, 0, 20]
           },
           axisLine: {             //坐标轴轴线相关设置。
             lineStyle: {
-              color: '#FA6F53',
+              color: 'black',
             }
           }
         },
         yAxis: {
           name: 'Number',
           nameTextStyle: {
-            color: '#FA6F53',
+            color: 'black',
             fontSize: 16,
             padding: [0, 0, 10, 0]
           },
           axisLine: {
             lineStyle: {
-              color: '#FA6F53',
+              color: 'black',
             }
           },
           type: 'value'
@@ -181,7 +181,7 @@ export default {
             type: 'line',               // 类型为折线图
             lineStyle: {                // 线条样式 => 必须使用normal属性
               normal: {
-                color: '#8AE09F',
+                color: 'blue',
               }
             },
           },
