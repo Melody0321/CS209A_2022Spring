266d1b35edea5961f411dc3ff564a72f64fbfce1
ttt-noora <72697265+ttt-noora@users.noreply.github.com>
2022-05-23 21:58:35 +0800

    Update ProjectDAO.java

--- back-end/ProjectDAO.java
+++ back-end/ProjectDAO.java
@@ -7,12 +7,14 @@ import java.util.List;
 public interface ProjectDAO extends JpaRepository<Project, Long> {
     List<Project> findAll();
     List<Project> findTop8ByOrderById();
+
     @Query(value="with a as(select distinct project_id as id from project_topic\n" +
-            "where topic like '%an%'\n" +
+            "where topic like ?1 \n" +
             "union\n" +
             "select distinct id\n" +
             "from project\n" +
-            "where name like '%an%')\n" +
-            "select * from project,a where project.id=a.id",nativeQuery = true)
-    List<Project> findByProjectInfo(String name, String tag);
+            "where name like ?1)\n" +
+            "select project.id as id, name, url, star, issues, watch, fork from project,a where project.id=a.id",nativeQuery = true)
+    List<Project> findByProjectInfo(String name);
+
 }
