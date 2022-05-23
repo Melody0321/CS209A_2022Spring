2042dddf2d635fd9034db0bd2a8785869876f039
ttt-noora <72697265+ttt-noora@users.noreply.github.com>
2022-05-23 22:01:34 +0800

    Update ProjectService.java

--- back-end/ProjectService.java
+++ back-end/ProjectService.java
@@ -1,32 +1,42 @@
 package com.example.demo.service;
 import com.example.demo.dao.ProjectDAO;
 import com.example.demo.pojo.Project;
+import com.example.demo.pojo.Topic;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectService {
     @Autowired
     ProjectDAO projectDAO;
-//    public List<Project> findAll(){
-//        return projectDAO.findAll();
-//    }
     public List<Project> findTop8(){
         return projectDAO.findTop8ByOrderById();
     }
-    public List<Project> findByProjectInfo(String name, String tag){
-        String realName="%";
-        String realTag="%";
-        if(Objects.equals(name, "")){
-            realName="%";
-            realTag="%"+tag+"%";
+    public List<Project> findByProjectInfo(String name, int sort){
+        String realName="%"+name+"%";
+        List<Project> re = projectDAO.findByProjectInfo(realName);
+        // by star
+        if (sort==1){
+            re.sort(((o1, o2) -> o2.getStar()-o1.getStar()));
         }
-        if (Objects.equals(tag, "")){
-            realTag="%";
-            realName="%"+name+"%";
+        // by fork
+        else if (sort==2){
+            re = re.stream().sorted(Comparator.comparing(Project::getFork).reversed()).collect(Collectors.toList());
         }
-        return projectDAO.findByProjectInfo(realName, realTag);
+        // by watch
+        else if (sort==3){
+            re.sort(((o1, o2) -> o2.getWatch()-o1.getWatch()));
+        }
+        //by issues
+        else if (sort==4){
+            re.sort(((o1, o2) -> o2.getIssues()-o1.getIssues()));
+        }
+        return re;
     }
+
 }
